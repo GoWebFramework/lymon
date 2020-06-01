e15e27b86f2e6c9de3ca55d94c729a217f71b5d3
cendol <14269809+codenoid@users.noreply.github.com>
2020-06-02 05:33:52 +0700

    [temp] helper/FormToMAP: only take first (0) value

--- helper.go
+++ helper.go
@@ -8,10 +8,10 @@ func FormToMAP(form url.Values) map[string]interface{} {
 	result := map[string]interface{}{}
 
 	for key, val := range form {
-		result[key] = val
+		if len(val) > 0 {
+			result[key] = val[0] // there will be someone who will reference issue to this line
+		}
 	}
 
 	return result
 }
-
-
